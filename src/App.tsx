4bce14030c436d09404b64f6e09987b8497b45cf
Shubham Verma <shubhamv@izooto.com>
2024-07-21 02:18:10 +0530

    @dgsrc---Created Three Spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 
 import ColorSpinner from './components/Spinner/ColorSpinner/ColorSpinner'
+import ThreeSpinner from './components/Spinner/ThreeSpinner/ThreeSpinner'
 
 const App = () => {
   return (
+    <>
     <ColorSpinner size='small' color='blue' />
+    <ThreeSpinner size='medium'/>
+    </>
   )
 }
 
