24bbf726505c4f9343ba5cc749febf1669d0dbf9
Shubham Verma <shubhamv@izooto.com>
2024-07-21 14:16:16 +0530

    @dgsrc -- Created ProgressBar and also improve file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+// import { useState, useEffect  } from 'react'
+import ColorSpinner from './components/Loaders/Spinner/ColorSpinner/ColorSpinner'
+import ThreeSpinner from './components/Loaders/Spinner/ThreeSpinner/ThreeSpinner'
+import ProgressBar from './components/Loaders/ProgressBar/ProgressBar'
 
-import ColorSpinner from './components/Spinner/ColorSpinner/ColorSpinner'
-import ThreeSpinner from './components/Spinner/ThreeSpinner/ThreeSpinner'
 
 const App = () => {
+
+
   return (
     <>
-    <ColorSpinner size='small' color='blue' />
+    <ColorSpinner size='medium' color='blue' />
     <ThreeSpinner size='medium'/>
+    <ProgressBar value={10} max={100} color='black' backgroundColor='blue' borderRadius={10} />
+
     </>
   )
 }
+  
 
 export default App
